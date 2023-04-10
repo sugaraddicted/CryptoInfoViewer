c8c90d498d0a2a78ea2c443c14f03dd048b1db0c
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-04-10 20:44:12 +0300

    added market model

---
+++ CryptoInfoViewer/MVVM/Model/MarketModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoInfoViewer.MVVM.Model
+{
+    internal class MarketModel
+    {
+        public string Name { get; set; }
+        public string Identifier { get; set; }
+        public bool HasTradingIncentive { get; set; }
+    }
+}
