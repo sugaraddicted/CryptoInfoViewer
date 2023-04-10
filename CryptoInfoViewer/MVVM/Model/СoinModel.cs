e2d0c02096e085b195fd9fdb7fffa66d1f2a103b
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-04-10 20:43:49 +0300

    added coin model

---
+++ CryptoInfoViewer/MVVM/Model/СoinModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoInfoViewer.MVVM.Model
+{
+    internal class СoinModel
+    {
+        public string Image { get; set; }
+        public long TotalVolume { get; set; }
+        public decimal PriceChange24h { get; set; }    
+        public decimal PriceChangePercentage24h { get; set; }
+    }
+}
